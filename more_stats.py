c8dcf0d2505b09fce87f530f1e5d9e15ab0adf7d
Noam <noam.barenholtz@yale.edu>
2026-08-10 16:25:31 -0400

    Building protein identifying

---
+++ more_stats.py
@@ -0,0 +1,17 @@
+# want to calculate total stats from existing data
+    # Total unique atom types
+    # Total rotatable bonds
+    # Avg number of rotatable bonds
+    # Avg number unique atoms
+
+# New stats to find
+    # Avg number of atoms
+    # Total number of atoms
+    # number atoms vs number rotatable bonds
+
+
+"""Calculating averages and totals from existing files"""
+# total atom types
+# 1. load atoms.csv
+# 2. number of rows - 1
+
